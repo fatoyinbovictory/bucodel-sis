8e70139155b28a1cd063e10248b347bc3102cbc0
Victory Fatoyinbo <vicfatoyinbo@gmail.com>
2023-03-02 15:35:32 +0100

    Created Admin dashboard

--- src/Pages/Admin/Dashboard.jsx
+++ src/Pages/Admin/Dashboard.jsx
@@ -6,7 +6,27 @@ const Dashboard = () => {
     <>
     <AdminNav/>
       <section className="profile-container">
-        <h1 className="landing-main-header-blue">hello</h1>
+        <h1 className="landing-main-header-blue">Welcome, XXX</h1>
+        <div className="box-row">
+          <section className="dashboard-box">
+            <h1>232</h1>
+            <h3>Total Students</h3>
+          </section>
+          <section className="dashboard-box">
+            <h1>17</h1>
+            <h3>Total Facilitators</h3>
+          </section>
+        </div>
+        <div className="box-row">
+          <section className="dashboard-box">
+            <h1>2023/2024.1</h1>
+            <h3>Current Semester</h3>
+          </section>
+          <section className="dashboard-box">
+            <h1>5</h1>
+            <h3>Total Programs</h3>
+          </section>
+        </div>
       </section>
     </>
   );
